49c6a3ed5ddbb56ca038b20ed4bc6878bb659583
ConstanceGlly <constanceguelluy@gmail.com>
2023-11-06 14:57:48 +0100

    eda

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+CATEGORICAL_COLs =[
+    "occupation",
+    "workclass",
+    "marital-status",
+    "relationship",
+    "race",
+    "native-country",
+    "gender",
+    "income",
+]
+NUMERICAL_COLs = [
+    "age",
+    "fnlwgt",
+    "educational-num",
+    "capital-gain",
+    "capital-loss",
+    "hours-per-week",
+]
+TARGET = ["income"]
+
+DROP_COL = []
